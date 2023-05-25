683b1eb7c90b80837e15da6e87e10cbbb671dcb6
jackbar13 <jackbardagji@gmail.com>
2023-05-25 20:02:29 +0200

    adding modules headers, declaration of functions MD5_read and MD5_write with comments

--- md5-c/driver/md5_module.c
+++ md5-c/driver/md5_module.c
@@ -2,12 +2,24 @@
 // Created by jbardaghji on 25/05/23.
 //
 #include <linux/init.h>
+#include <linux/config.h>
 #include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/device.h>
-
-
-
+#include <linux/kernel.h> /* printk() */
+#include <linux/slab.h> /* kmalloc() */
+#include <linux/fs.h> /* everything... */
+#include <linux/errno.h> /* error codes */
+#include <linux/types.h> /* size_t */
+#include <linux/proc_fs.h>
+#include <linux/fcntl.h> /* O_ACCMODE */
+#include <asm/system.h> /* cli(), *_flags */
+#include <asm/uaccess.h> /* copy_from/to_user */
+/*
+The specific value of LEN_OF_KERNEL_BUFFER will depend on various factors,
+including the requirements of your MD5 cryptocore,
+the expected maximum size of the data to be processed,
+and any constraints imposed by the underlying hardware or system
+*/
+#define LEN_OF_KERNEL_BUFFER 1024  // Specify a buffer size of 1024 bytes
 #define  DEVICE_NAME "MD5_module"
 #define  CLASS_NAME  "MD5"
 
@@ -22,8 +34,123 @@ static ssize_t MD5_read(struct file *, char *, size_t, loff_t *);
 static ssize_t MD5_write(struct file *, const char *, size_t, loff_t *);
 static long int MD5_ioctl(struct file *, unsigned int , unsigned long );
 
+static struct file_operations fops =
+        {
+                .owner = THIS_MODULE,
+                .open = MD5_open_close,
+                .read = MD5_read,
+                .write = MD5_write,
+                .release = MD5_open_close,
+                .unlocked_ioctl = MD5_ioctl,
+                .compat_ioctl = MD5_ioctl
+        };
+
+static long int MD5_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
+    /*
+    switch (cmd) {
+        case BLINK_RATE:
+            selected_register = BLINK_RATE;
+            break;
+        case ENABLE:
+            selected_register = ENABLE;
+            break;
+    }
+    */
+
+    printk(KERN_INFO "LM: Executing IOCTL\n");
+
+    return -1;
+}
+
+
+/** @brief The device release function that is called whenever the device is initialized/released by
+ *  the userspace program
+ *  @param inodep A pointer to an inode object (defined in linux/fs.h)
+ *  @param filep A pointer to a file object (defined in linux/fs.h)
+ */
+
+static int  MD5_open_close(struct inode* inodep, struct file * filep) {
+    /*
+    selected_register = ENABLE;
+    WRITE_DATA_TO_THE_HW(0);
+    selected_register = BLINK_RATE;
+    WRITE_DATA_TO_THE_HW(0);
+    */
+
+    printk(KERN_INFO "LM: Executing OPENRELEASE\n");
+    return 0;
+
+}
+
+
+/** @brief This function is called whenever device is being read from user space i.e. data is
+ *  being sent from the device to the user. In this case is uses the copy_to_user() function to
+ *  send the buffer string to the user and captures any errors.
+ *  @param filep A pointer to a file object (defined in linux/fs.h)
+ *  @param buffer The pointer to the buffer to which this function writes the data
+ *  @param len The length of the b
+ *  @param offset The offset if required
+ */
+static ssize_t MD5_read(struct file * filep, char * buffer, size_t len, loff_t * offset) {
+
+    char kernel_data[LEN_OF_KERNEL_BUFFER]; // Define a kernel buffer to hold the data, it should be replaced later with other variable in the driver structure
+    size_t data_length; // Variable to store the length of the data obtained
+
+    // Perform necessary operations to obtain the data from the MD5 cryptocore
+    // For example, retrieve computed MD5 hash or other relevant information
+
+    // Copy the data from kernel space to user space
+    if (copy_to_user(buffer, kernel_data, data_length))
+    {
+        return -EFAULT; // Error occurred while copying data to user space
+    }
+
+    // Return the number of bytes read
+    return data_length;
+
+    /*int data;
+    READ_DATA_FROM_THE_HW (&data);
+    copy_to_user(buffer, &data, 4);*/
+    //printk(KERN_INFO "LM: Executing READ\n");
+    //return 4;
+}
+
+
+/** @brief This function is called whenever the device is being written to from user space i.e.
+ *  data is sent to the device from the user.
+ *  @param file A pointer to a file object
+ *  @param buffer The buffer to that contains the string to write to the device
+ *  @param len The length of the array of data that is being passed in the const char buffer
+ *  @param offset The offset if required
+ */
+
+/*
+ * the data is copied from user space to a kernel buffer using the copy_from_user function to ensure safe memory access.
+ * Afterward, we can process the data using the MD5 cryptocore,performing any necessary computations or data handling.
+ */
+static ssize_t MD5_write(struct file * filep, const char * buffer, size_t len, loff_t * offset) {
+    // Validate and copy the user-provided data to kernel space
+    char kernel_buffer[LEN_OF_KERNEL_BUFFER];
+    if (copy_from_user(kernel_buffer, buffer, len))
+    {
+        return -EFAULT; // Error occurred while copying data from user space
+    }
+
+    // Process the data using the MD5 cryptocore
+    // Perform necessary computations or data handling
+
+    // Return the number of bytes written
+    return len;
+
+    //WRITE_DATA_TO_THE_HW( buffer);
+    //printk(KERN_INFO "LM: Executing WRITE\n");
+    //return 1;
+}
+
+
+
 // Function to initialize the MD5 cryptocore device driver
-static int md5_cryptocore_driver_init(void)
+static int __init md5_cryptocore_driver_init(void)
 {
     // Perform necessary initialization specific to QEMU here
     // For example, set up virtual memory-mapped interfaces or QEMU-specific configurations
@@ -42,7 +169,7 @@ static int md5_cryptocore_driver_init(void)
 }
 
 // Function to clean up and unregister the MD5 cryptocore device driver
-static void md5_cryptocore_driver_exit(void)
+static void __exit md5_cryptocore_driver_exit(void)
 {
     // Release any resources allocated during initialization
     // For example, free memory, release IRQ, etc.
