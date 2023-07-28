7565df2ef0833e056e662392b252b841bb3cbf43
jackbar13 <jackbardagji@gmail.com>
2023-07-28 22:30:00 +0200

    adding the driver

---
+++ driver/crypto-chrdev.c
@@ -0,0 +1,80 @@
+#include <linux/cdev.h>
+#include <linux/poll.h>
+#include <linux/sched.h>
+#include <linux/module.h>
+#include <linux/wait.h>
+#include <linux/virtio.h>
+#include <linux/virtio_config.h>
+
+#include "crypto.h"
+#include "crypto-chrdev.h"
+#include "debug.h"
+
+#include "cryptodev.h"
+
+#define OP_CIOCGSESSION 0
+#define OP_CIOCFSESSION 1
+#define OP_CIOCCRYPT 2
+
+/*
+ * Global data
+ */
+struct cdev crypto_chrdev_cdev;
+
+/*************************************
+ * Implementation of file operations
+ *************************************/
+
+
+
+static struct file_operations crypto_chrdev_fops = 
+{
+	.owner          = THIS_MODULE,
+	.open           = crypto_chrdev_open,
+	.release        = crypto_chrdev_release,
+	.read           = crypto_chrdev_read,
+	.unlocked_ioctl = crypto_chrdev_ioctl,
+};
+
+int crypto_chrdev_init(void)
+{
+	int ret;
+	dev_t dev_no;
+	unsigned int crypto_minor_cnt = CRYPTO_NR_DEVICES;
+	
+	debug("Initializing character device...");
+	cdev_init(&crypto_chrdev_cdev, &crypto_chrdev_fops);
+	crypto_chrdev_cdev.owner = THIS_MODULE;
+	
+	dev_no = MKDEV(CRYPTO_CHRDEV_MAJOR, 0);
+	ret = register_chrdev_region(dev_no, crypto_minor_cnt, "crypto_devs");
+	if (ret < 0) {
+		debug("failed to register region, ret = %d", ret);
+		goto out;
+	}
+	ret = cdev_add(&crypto_chrdev_cdev, dev_no, crypto_minor_cnt);
+	if (ret < 0) {
+		debug("failed to add character device");
+		goto out_with_chrdev_region;
+	}
+
+	debug("Completed successfully");
+	return 0;
+
+out_with_chrdev_region:
+	unregister_chrdev_region(dev_no, crypto_minor_cnt);
+out:
+	return ret;
+}
+
+void crypto_chrdev_destroy(void)
+{
+	dev_t dev_no;
+	unsigned int crypto_minor_cnt = CRYPTO_NR_DEVICES;
+
+	debug("entering");
+	dev_no = MKDEV(CRYPTO_CHRDEV_MAJOR, 0);
+	cdev_del(&crypto_chrdev_cdev);
+	unregister_chrdev_region(dev_no, crypto_minor_cnt);
+	debug("leaving");
+}
